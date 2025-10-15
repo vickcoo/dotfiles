d330f7d541e10c2e10210a8cfae8f3a81e47a651
Vickcoo <cwk91405133@gmail.com>
2025-10-15 22:40:02 +0800

    add ios development plugins

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,79 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    { "antosha417/nvim-lsp-file-operations", config = true },
+  },
+  config = function()
+    local lspconfig = require("lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local keymap = vim.keymap -- for conciseness
+    local opts = { noremap = true, silent = true }
+    local on_attach = function(_, bufnr)
+      opts.buffer = bufnr
+
+      -- set keybindings
+      opts.desc = "Show LSP definitions"
+      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions trim_text=true<cr>", opts)
+
+      opts.desc = "Show LSP definitions in split"
+      keymap.set("n", "gD", "<cmd>vsplit | Telescope lsp_definitions trim_text=true<cr>", opts)
+
+      opts.desc = "Show LSP references"
+      vim.keymap.set(
+        "n",
+        "gr",
+        "<cmd>Telescope lsp_references trim_text=true include_declaration=false<cr>",
+        opts
+      )
+
+      opts.desc = "Show LSP implementation"
+      vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<cr>", opts)
+
+      opts.desc = "Show LSP code actions"
+      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+
+      opts.desc = "Smart rename"
+      keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+      opts.desc = "Show buffer diagnostics"
+      keymap.set("n", "<leader><leader>d", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+
+      opts.desc = "Go to previous diagnostic"
+      keymap.set("n", "[d", function()
+        vim.diagnostic.jump({ count = -1 })
+        vim.cmd("normal! zz")
+      end, opts)
+
+      opts.desc = "Go to next diagnostic"
+      keymap.set("n", "]d", function()
+        vim.diagnostic.jump({ count = 1 })
+        vim.cmd("normal! zz")
+      end, opts)
+
+      opts.desc = "Show documentation for what is under cursor"
+      keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+      opts.desc = "Restart LSP"
+      keymap.set("n", "<leader>rl", ":LspRestart | LspStart<CR>", opts)
+    end
+
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+
+    local defaultLSPs = {
+      "sourcekit",
+    }
+
+    for _, lsp in ipairs(defaultLSPs) do
+      lspconfig[lsp].setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        cmd = lsp == "sourcekit" and { vim.trim(vim.fn.system("xcrun -f sourcekit-lsp")) } or nil,
+      })
+    end
+
+    opts.desc = "Show line diagnostics"
+    keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+  end,
+}
---
+++ lua/plugins/oil-nvim.lua
@@ -0,0 +1,47 @@
+return {
+    {
+        'stevearc/oil.nvim',
+        enabled = false,
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {},
+        -- Optional dependencies
+        dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+        -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+        -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+        lazy = false,
+        config = function()
+            require("oil").setup({
+                default_file_explorer = true,
+                columns = {
+                    "icon",
+                    -- "permissions",
+                    -- "size",
+                    -- "mtime",
+                },
+                delete_to_trash = false,
+                watch_for_changes = false,
+                keymaps = {
+                    ["g?"] = { "actions.show_help", mode = "n" },
+                    ["<CR>"] = "actions.select",
+                    ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+                    ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+                    ["<C-t>"] = { "actions.select", opts = { tab = true } },
+                    ["<C-p>"] = "actions.preview",
+                    ["<C-c>"] = { "actions.close", mode = "n" },
+                    ["<C-l>"] = "actions.refresh",
+                    ["-"] = { "actions.parent", mode = "n" },
+                    ["_"] = { "actions.open_cwd", mode = "n" },
+                    ["`"] = { "actions.cd", mode = "n" },
+                    ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+                    ["gs"] = { "actions.change_sort", mode = "n" },
+                    ["gx"] = "actions.open_external",
+                    ["g."] = { "actions.toggle_hidden", mode = "n" },
+                    ["g\\"] = { "actions.toggle_trash", mode = "n" },
+                },
+                use_default_keymaps = true,
+            })
+            vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+        end
+    }
+}
