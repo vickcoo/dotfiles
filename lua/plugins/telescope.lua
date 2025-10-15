a808940cfafd6a26376330b15799f4742f18d809
Vickcoo <cwk91405133@gmail.com>
2025-10-15 22:41:28 +0800

    add keymap to list all normal mode keymappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,9 +5,10 @@ return {
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<leader><leader>', builtin.keymaps, { desc = 'Telescope lists all normal mode keymappings' })
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
             vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
-            vim.keymap.set('n', '<leader>ps', function()
+            vim.keymap.set('n', '<leader>fs', function()
                 builtin.grep_string({ search = vim.fn.input("Grep > ") });
             end)
         end,
