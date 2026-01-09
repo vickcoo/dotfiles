f8c8d6e1689f143f33f90dae0fcd2d44e43e7b89
Vickcoo <cwk91405133@gmail.com>
2026-01-09 14:06:28 +0800

    feat(nvim): add quickly move up/down

--- .config/nvim/lua/vickcoo/remap.lua
+++ .config/nvim/lua/vickcoo/remap.lua
@@ -4,6 +4,8 @@ vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true, desc = "vick
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "vickcoo: scroll down half page and center" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "vickcoo: scroll up half page and center" })
+vim.keymap.set("n", "J", "5j", { desc = "vickcoo: scroll down by 5 lines rapidly" })
+vim.keymap.set("n", "K", "5k", { desc = "vickcoo: scroll up by 5 lines rapidly" })
 
 vim.keymap.set("x", "<leader>p", "\"_dP", { desc = "vickcoo: paste without yanking" })
 
