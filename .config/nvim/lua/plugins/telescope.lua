045e01290e20dccedb7f3780d4c9eb08d5e481b2
Vickcoo <cwk91405133@gmail.com>
2026-03-27 17:26:01 +0800

    feat(nvim): add shortcut to list all changes file

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader><leader>', builtin.keymaps, { desc = 'telescope: telescope lists all normal mode keymappings' })
             vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'telescope: telescope find files' })
+            vim.keymap.set('n', '<leader>fg', builtin.git_status, { desc = 'telescope: list all changes file' })
             vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'telescope: telescope find git files' })
             vim.keymap.set('n', '<leader>fs', function()
                 builtin.grep_string({ search = vim.fn.input("Grep > ") });
